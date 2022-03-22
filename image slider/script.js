7e73d7438243fde82ed1fa2f1bfe34b89f7c130f
manavshah07 <manavshah140@gmail.com>
2022-03-22 11:27:31 +0530

    css3 docs

---
+++ image slider/script.js
@@ -0,0 +1,12 @@
+var cars = ["image1", "image2", "image3"];
+document.getElementById("img-slide").innerHTML = cars;
+
+function msFunction() {
+    var image1=document.getElementById("img-slide").src = "Image/paris.jpg";
+}
+function myFunction(){
+    var image2=document.getElementById("img-slide").src = "Image/sanfran.jpg";
+}
+function ourFuction(){
+    var image3=document.getElementById("img-slide").src = "Image/map.jpg";
+}
---
+++ image slider/tp.js
@@ -0,0 +1,10 @@
+if($(".btn").click())
+$(".btn").click(function(){
+	var img2 = "Image/Image1.jpg";
+	$(".hero-slider").css("background-image","url(" + img2+ ")");
+});
+$(".btn").click(function(){
+	var img3 = "Image/Image2.jpg";
+	$(".hero-slider").css("background-image","url(" + img3 + ")");
+});
+
